529f0ddf6ca5c1bf5a91c1fdf77337f449bfd68f
josueNoha <nohajosue@gmail.com>
2022-11-02 18:47:12 -0500

    retoques diagrama flexocompresion

--- concreto/columnas/columnas_utils.py
+++ concreto/columnas/columnas_utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
-import matplotlib as plt
+import matplotlib.pyplot as plt
+
 
 def def_units():
     global N , m, cm, Pa, MPa, pulg
@@ -29,6 +30,7 @@ def def_rebar():
 def_units()
 def_rebar()
 
+
 def create_rebar_matrix(d_p,d_s,n_f,n_c):
     reb_mat = [d_p] + [d_s]*(n_c-2) + [d_p]
     for i in range(n_f -2 ):
@@ -38,6 +40,11 @@ def create_rebar_matrix(d_p,d_s,n_f,n_c):
     reb_mat = np.vstack([reb_mat,fila])
     return reb_mat
 
+def vector_function(function,C,*args):
+    vect_f = [function(*args,c) for c in C]
+    return np.array(vect_f)
+
+#Vector de distancias a las varillas de refuerzos
 def dist_vector(b,r,d_p,d_st,n):
     sep = (b-2*(r+d_st)-d_p)/(n-1)
     reb_vect = [r+d_p/2+d_st+i*sep for i in range(n)]
@@ -51,7 +58,7 @@ def def_betha(fc):
     else:
         return 0.65
     
-def def_phi(c,d_t,eps_u,eps_y):
+def def_phi(d_t,eps_u,eps_y,c):
     if c==0:
         return 0.9
     eps_t = (c-d_t)/c*eps_u
@@ -63,12 +70,8 @@ def def_phi(c,d_t,eps_u,eps_y):
     else:
         return phi
     
-def vector_phi(C,d_t,eps_u,eps_y):
-    vect_phi = [def_phi(c,d_t,eps_u,eps_y) for c in C]
-    return np.array(vect_phi)
-
-#esfuerzo en cada fila de acero:
-def stress_f(x,c,eps_u,Es,fy):
+#Esfuerzo en barras de acero segun su posición:
+def stress_f(eps_u,Es,fy,c,x):
     if c==0:
         return -fy
     fs = eps_u*(c-x)/c*Es
@@ -77,15 +80,7 @@ def stress_f(x,c,eps_u,Es,fy):
     else:
         return fs
     
-def vect_stress_f(dist_vect,c,eps_u,Es,fy):
-    fs_vect = [stress_f(x,c,eps_u,Es,fy) for x in dist_vect]
-    return np.array(fs_vect)
-
-def matrix_fs(C,dist_vect,eps_u,Es,fy):
-    mat_fs = [vect_stress_f(dist_vect,c,eps_u,Es,fy) for c in C]
-    return np.array(mat_fs)
-
-def comp_area(b,h,a,theta):
+def comp_area(b,h,theta,a):
     if theta == 0:
         return a*h,a/2,h/2
     elif theta == math.pi/2:
@@ -113,16 +108,7 @@ def comp_area(b,h,a,theta):
     
     return A_c, x_c, y_c
     
-def vect_comp_area(b,h,a,theta):
-    vect_A_comp = []
-    vect_c_x = []
-    vect_c_y = []
-    for a_i in a:
-        A_c,x_c,y_c = comp_area(b,h,a_i,theta)
-        vect_A_comp.append(A_c)
-        vect_c_x.append(x_c)
-        vect_c_y.append(y_c)
-    return np.array(vect_A_comp),np.array(vect_c_x),np.array(vect_c_y)
+
 
 def ax_force_n(A_comp,rsp,fc):
     return 0.85*fc*A_comp+rsp
@@ -142,39 +128,94 @@ def vect_momentum_n(A_comp,x_c,rsm,fc,b,theta):
         vect_Mn.append(momemtum_n(A_comp_i,x_c[i],rsm[i],fc,b,theta))
     return np.array(vect_Mn)
 
-def flex_comp_data(fc,eps_u,fy,Es,eps_y,b,h,r,d_p,d_s,d_st,n_f,n_c,phi,theta):
+# Concreto
+fc  = 21* MPa  #Resistencia a la compresion
+eps_u = 0.003 #Deformación unitaria ultima
+
+#Acero
+fy = 420 * MPa #Esfuerzo a la fluencia del acero
+Es = 200000 * MPa #Módulo de elasticidad del acero
+eps_y = 0.0021 #Deformación de fluencia del acero
+
+b = 35 * cm
+h = 75 * cm
+r = 4 * cm #recubrimiento
+Ag = b*h #Área bruta
+
+#Refuerzo en la columna:
+d_p = d_5 #diámetro principal
+d_s = d_5 #diámetro secundario
+d_st = d_3 #diámetro del estribo
+n_f = 6 #filas de acero
+n_c = 3 #columnas de acero
+
+
+def flex_comp_data(d_conc,d_steel,geom,steel,phi,theta):
+    fc = d_conc['fc']
+    eps_u = d_conc['eps_u']
+    
+    fy = d_steel['fy']
+    Es = d_steel['Es']
+    eps_y = d_steel['eps_y']
+    
+    b = geom['b']
+    h = geom['h']
+    r = geom['r']
     Ag = b*h
+    
+    d_p = steel['d_p']
+    d_s = steel['d_s']
+    d_st = steel['d_st']
+    n_f = steel['n_f']
+    n_c = steel['n_c']
+    
     reb_matx = create_rebar_matrix(d_p,d_s,n_f,n_c)
     area_matx = reb_matx**2*math.pi/4
     area_vect = area_matx.flatten()
+    
     Aref = area_matx.sum()
+    
     dist_vect_x = dist_vector(b, r, d_p, d_st,n_c)
     dist_vect_y = dist_vector(h, r, d_p, d_st,n_f)
+    
     P_n = 0.85*fc*(Ag-Aref)+Aref*fy
     phiP_n = phi*0.8*P_n
     betha = def_betha(fc)
+    
     theta = math.radians(theta)
     l = b*math.cos(theta)+h*math.sin(theta)
     a = np.array([i/100*l for i in range(100)])
     c = a/betha
+    
     dist_vect_x_rot = dist_vect_x*math.cos(theta)
     dist_vect_y_rot = dist_vect_y*math.sin(theta)
     dist_matx = np.array([i+dist_vect_x_rot for i in dist_vect_y_rot])
     dist_vect = dist_matx.flatten()
-    matx_fs = matrix_fs(c,dist_vect,eps_u,Es,fy)
+    
+    matx_fs = vector_function(vector_function,c,stress_f,dist_vect,eps_u,Es,fy)
+    
     d_t = dist_vect.max()
-    vect_phi = vector_phi(c,d_t,eps_u,eps_y)
+    
+    vect_phi = vector_function(def_phi,c,d_t,eps_u,eps_y)
+    
     rsp = np.dot(matx_fs,area_vect)
+    
     dist_list_x = np.array([dist_vect_x for _ in dist_vect_y]).flatten()
     dist_list_y = np.array([i for i in dist_vect_y for _ in dist_vect_x])
     dist_CM_x = b/2-dist_list_x
     dist_CM_y = h/2-dist_list_y
     rsm_x = np.dot(matx_fs*area_vect,dist_CM_x)
     rsm_y = np.dot(matx_fs*area_vect,dist_CM_y)
-    A_c,x_c,y_c = vect_comp_area(b,h,a,theta)
+    
+    A_X_Y = vector_function(comp_area,a,b,h,theta)
+    A_c = A_X_Y[:,0]
+    x_c = A_X_Y[:,1]
+    y_c = A_X_Y[:,2]
+    
     vect_Pn = vect_ax_force_n(A_c,rsp,fc)
     vect_phi_Pn = vect_phi*vect_Pn
     vect_phi_Pn = np.array([i if i < phiP_n else phiP_n for i in vect_phi_Pn])
+    
     vect_Mn_x = vect_momentum_n(A_c,x_c,rsm_x,fc,b,theta)
     vect_Mn_y = vect_momentum_n(A_c,y_c,rsm_y,fc,h,theta)
     vect_phi_Mn_x = vect_phi*vect_Mn_x
@@ -191,47 +232,10 @@ def plot_flex_comp(vect_Pn,vect_Mn_x,vect_Mn_y,ax):
             ,linestyle = ':',alpha=0.7,color='#23987C')
     ax.plot(-vect_Mn_x/10**6,vect_Mn_y/10**6, vect_Pn/1000
             ,linestyle = ':',alpha=0.7,color='#23987C')
-    
+
   
 if __name__ == '__main__':
-    def_units()
-    def_rebar()
-    fc=21*MPa
-    d_p = 0.015875
-    d_s = 0.015875
-    d_st = 0.009525
-    n_f = 6
-    n_c = 3
-    reb_mat = create_rebar_matrix(d_p,d_s,n_f,n_c)
-    A_mat = reb_mat**2/4*math.pi
-    A_ref = reb_mat.sum()
-    b=0.35
-    h=0.75
-    r=0.04
-    dist_vect_x = dist_vector(b, r, d_p, d_st,n_c)
-    dist_vect_y = dist_vector(h, r, d_p, d_st,n_f)  
-    betha = def_betha(fc)
-    c = 0.35
-    d_t = dist_vect_x[-1]
-    eps_u = 0.003
-    eps_y = 0.0021
-    phi = def_phi(c,d_t,eps_u,eps_y)
-    fy = 420*MPa
-    Es = 200000*MPa
-    x=0.2925375
-    fs = stress_f(eps_u,c,x,Es,fy)
-    fs_vect = vect_stress_f(dist_vect_y, c, eps_u, Es, fy)
-    a = np.dot(fs_vect,A_mat).sum()
-    a_x = np.array([i/100*b for i in range(101)])
-    C_x = a_x/betha
-    mat_fsx = matrix_fs(C_x,dist_vect_x,eps_u,Es,fy)/10**6
-    theta = math.radians(10)
-    c = 40*cm
-    X = comp_area(b,h,c,theta)
-    l = b*math.cos(theta)+h*math.sin(theta)
-    #a = np.array([i/100*l for i in range(100)])
-    a = np.array([0,5,10,15,20,25,30,40,45])*cm
-    x_A_c,x_c,y_c = vect_comp_area(b,h,a,theta)
+    pass
     
     
 
