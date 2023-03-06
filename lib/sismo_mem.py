f4ca22929540de8456aa57628fa7dccd75cd3a2e
josueNoha <nohajosue@gmail.com>
2023-03-06 17:54:53 -0500

    correccion sismo_mem

--- lib/sismo_mem.py
+++ lib/sismo_mem.py
@@ -492,39 +492,44 @@ if __name__ == '__main__':
                 'Albañilería Armada o Confinada',
                 'Madera']
 
-    datos = {'Factor de Importancia': 'C',
-            'Sistema Estructural': sistemas[0],
-            'Número de Pisos': '4',
-            'Número de Sotanos': '0',
-            'Número de Azoteas': '0',
-            'Factor Zona': '2',
-            'Factor Suelo': 'S2',
-            'Piso Blando': 'False',
-            'Piso Blando Extremo': 'False',
-            'Irregularidad de Masa': 'False',
-            'Irregularidad Vertical': 'False',
-            'Dicontinuidad Vertical': 'False',
-            'Dicontinuidad Vertical Extrema': 'False',
-            'Irregularidad Torsional': 'False',
-            'Irregulariad Torsional Extrema': 'False',
-            'Esquinas Entrantes': 'False',
-            'Discontinuidad del diafragma': 'False',
-            'Sistemas no Paralelos': 'False'}
+    categorias = ['A1 aislado',
+                  'A1 no aislado',
+                  'A2',
+                  'B',
+                  'C']
     
-    seism_loads = {'Sismo_EstX': 'Sx',
+    sis_loads = {'Sismo_EstX': 'Sx',
                  'Sismo_EstY': 'Sy',
                  'Sismo_DinX': 'SDx',
-                 'Sismo_DinY': 'SDy'
-        }
+                 'Sismo_DinY': 'SDy'}
 
-    sismo = sis.sismo_e30(data=datos)
-    sismo.seism_loads = seism_loads
-    # sismo.show_params()
+    zona = 4
+    suelo = 'S1'
+    sist_x = sistemas[0]
+    sist_y = sistemas[1]
+    categoria = categorias[4]
+    n_pisos = 4
+    n_sotanos = 0
+    n_azoteas = 0
+
+    sismo = sis.Sismo_e30()
+    sismo.data.factor_zona(zona)
+    sismo.data.factor_suelo(suelo)
+    sismo.data.periodos_suelo()
+    sismo.data.sist_estructural(sist_x,sist_y)
+    sismo.data.categoria_edificacion(categoria)
+    sismo.data.set_pisos(n_pisos,n_azoteas,n_sotanos)
+    sismo.data.irreg_altura(i_vertical=True)
+    sismo.data.irreg_planta(i_torsional=True)
+    sismo.data.factor_R()
+    sismo.data.set_pisos(n_pisos, n_azoteas, n_sotanos)
+    sismo.data.show_params()
+    
+    sismo.loads.set_seism_loads(sis_loads)
+    
     sismo.analisis_sismo(_SapModel)
     
-    zona = 1
-    suelo = 'S1'
-    categoria = 'A2'
+
 
     geometry_options = { "left": "2.5cm", "top": "1.5cm" }
     doc = Document(geometry_options=geometry_options)
@@ -544,15 +549,15 @@ if __name__ == '__main__':
     sist_estructural(s1, insert=coments, o_type=Subsection)
     factor_amplificacion(s1, insert=coments, o_type=Subsection)
     factor_importancia(s1,categoria)
-    table = sismo.modal
+    table = sismo.tables.modal
     ana_modal(s1, table, insert=coments, o_type=Subsubsection)
-    tabla = sismo.piso_blando_table
+    tabla = sismo.tables.piso_blando_table
     sis_x = tabla[tabla['OutputCase']=='SDx Max']
     sis_y = tabla[tabla['OutputCase']=='SDy Max']
     irreg_rigidez(s1,sis_x,sis_y, insert=coments, o_type=Subsubsection)
-    masa = sismo.rev_masa_table
+    masa = sismo.tables.rev_masa_table
     irreg_masa(s1,masa, insert=coments, o_type=Subsection)
-    tabla = sismo.torsion_table
+    tabla = sismo.tables.torsion_table
     sis_x = tabla[tabla['OutputCase']=='SDx Max']
     sis_y = tabla[tabla['OutputCase']=='SDy Max']
     s1.append(NoEscape(r'\newpage'))
